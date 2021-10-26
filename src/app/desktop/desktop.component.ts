382fb67140f9d4bee152e6df5a4d94c5ab1ea1d2
Daniel Wiehl <daniel.wiehl@sbb.ch>
2021-10-26 21:34:37 +0200

    2021-10-27-training-skeleton

---
+++ src/app/desktop/desktop.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-desktop',
+  templateUrl: './desktop.component.html',
+  styleUrls: ['./desktop.component.scss'],
+})
+export class DesktopComponent {
+}
